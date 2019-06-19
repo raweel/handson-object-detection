9f70032b71f9d735eb12c0334103e47fad679713
raweel <ooyamada.ootarou@gmail.com>
2019-06-19 11:32:47 +0000

    isaax sample 3rd

--- sample.py
+++ sample.py
@@ -12,6 +12,7 @@ net = cv2.dnn.readNetFromCaffe('/home/pi/models/MobileNetSSD_deploy.prototxt',
         '/home/pi/models/MobileNetSSD_deploy.caffemodel')
 
 def detect(frame):
+    # モデルが期待する形状に画像データを前処理する
     frame = cv2.resize(frame, (300, 300))
     blob = cv2.dnn.blobFromImage(
         image=frame,
@@ -20,28 +21,33 @@ def detect(frame):
         mean=127.5
     )
 
-net.setInput(blob)
-out = net.forward()
+    # データの入力・結果の取り出し
+    net.setInput(blob)
+    out = net.forward()
+    # 出力結果の取り出し
+    boxes = out[0,0,:,3:7] * np.array([300, 300, 300, 300])
+    classes = out[0,0,:,1]
+    confidences = out[0,0,:,2]
+    for i, box in enumerate(boxes):
+        # 20%以上の精度のボックスを取り出し
+        confidence = confidences[i]
+        if confidence < 0.2:
+            continue
+        # クラスの取り出し。データセットより、15はperson
+        idx = int(classes[i])
+        if idx != 15:
+            continue
 
-boxes = out[0,0,:,3:7] * np.array([300, 300, 300, 300])
-classes = out[0,0,:,1]
-confidences = out[0,0,:,2]
+        # 認識座標の取り出し
+        (startX, startY, endX, endY) = box.astype('int')
+        # 認識座標の描画
+        cv2.rectangle(frame, (startX, startY), (endX, endY), (0, 255, 0), 2)
+        # 認識結果の描画
+        label = '{}: {:.2f}%'.format('Person', confidence * 100)
+        y = startY - 15 if startY - 15 > 15 else startY + 15
+        cv2.putText(frame, label, (startX, y), cv2.FONT_HERSHEY_SIMPLEX, 0.5, (0, 255, 0), 1)
 
-for i, box in enumerate(boxes):
-    confidence = confidences[i]
-    if confidence < 0.2:
-        continue
-    idx = int(classes[i])
-    if idx != 15:
-        continue
-
-    (startX, startY, endX, endY) = box.astype('int')
-    cv2.rectangle(frame, (startX, startY), (endX, endY), (0, 255, 0), 2)
-
-    label = '{}: {:.2f}%'.format('Person', confidence * 100)
-    y = startY - 15 if startY - 15 > 15 else startY + 15
-    cv2.putText(frame, label, (startX, y), cv2.FONT_HERSHEY_SIMPLEX, 0.5, (0, 255, 0), 1)
-return frame
+    return frame
 
 def gen(camera):
     while True:
